fb0cbce669d510ce52443fdbdbff4ddb464ab15e
eafalkens <alicia-fs@outlook.com>
2026-02-07 13:49:24 +0100

    feature: implement addItem andd total price

--- src/main/java/com/example/shop/ShoppingCart.java
+++ src/main/java/com/example/shop/ShoppingCart.java
@@ -1,6 +1,13 @@
 package com.example.shop;
 
 public class ShoppingCart {
+    private double totalPrice = 0;
+
+    public void addItem(String item, double price, int quantity) {
+        totalPrice += price * quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
 }
-//Rimlig metod?:
-//addItem(String itemId, double price, int quantity)
